532e44b278219f99dc8e6d82aa9ec6fc3bec3a07
ZS-EG <grannygrandpacool@gmail.com>
2024-02-19 12:20:13 +0100

    Losuje i ustawia przycisk?, dodaje i usuwa z listy

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -2,31 +2,82 @@ package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonReset, buttonZapisz;
+    private Button buttonReset, buttonZapisz, buttonStart;
     private ImageButton imageButton;
     private ListView listaWynikow;
     private ArrayList<String> wyniki = new ArrayList<>();
+    private TextView textViewPunkty, textViewCzas;
+    private int sekundy = 10;
+    private int punkty = 0;
+    private LinearLayout linear;
+    private CountDownTimer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonReset = findViewById(R.id.buttonRestart);
         buttonZapisz = findViewById(R.id.buttonZapiszList);
+        buttonStart = findViewById(R.id.buttonStart);
         imageButton = findViewById(R.id.imageButton);
         listaWynikow = findViewById(R.id.listView);
+        textViewCzas = findViewById(R.id.textViewCzas);
+        textViewPunkty = findViewById(R.id.textViewPunkty);
+        linear = findViewById(R.id.linear);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                wyniki
+        );
+        buttonStart.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        timer = new CountDownTimer(sekundy*1000,1000) {
+                            @Override
+                            public void onTick(long l) {
+                                sekundy = (int) l/1000;
+                                textViewCzas.setText(String.valueOf(sekundy));
+                            }
+                            @Override
+                            public void onFinish() {
+                                textViewCzas.setText("Koniec czasu.");
+                                buttonZapisz.isEnabled();
+                            }
+                        };
+                    }
+                }
+        );
         buttonZapisz.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //zapis wyniku do listy
+                        //zapis wyniku do listy, tylko po ukonczonym czasie
+                        String wynik = textViewPunkty.getText().toString();
+                        wyniki.add(wynik);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+        );
+        listaWynikow.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                        wyniki.remove(i);
+                        adapter.notifyDataSetChanged();
                     }
                 }
         );
@@ -34,7 +85,12 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //reset wyniku
+                        //reset wyniku, i czasu
+                        imageButton.setLeft(120);
+                        imageButton.setTop(120);
+                        textViewCzas.setText("0 : 10");
+                        textViewPunkty.setText("0");
+                        punkty = 0;
                     }
                 }
         );
@@ -42,10 +98,22 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //zmienia pozycje po kazdym kliknieciu
+                        //zmienia pozycje po kazdym kliknieciu, losowanie 250-max, 10-min
+                        int losTop = (int)(Math.random()*250)+10;
+                        int losLeft = (int)(Math.random()*250)+10;
+                        imageButton.setTop(losTop);
+                        imageButton.setLeft(losLeft);
+                        punkty++;
+                        textViewPunkty.setText(punkty);
                     }
                 }
         );
-        //todo przycisk zmeinia pozycje po kazdym kliknieciu, liczenie punktow na kazdym kliknieciu, czas - 10 sekund, zapisz wynik do listy,
+        //todo
+        // przycisk zmeinia pozycje po kazdym kliknieciu (marginem left i top) - losowanie
+        // liczenie punktow
+        // czas - 10 sekund,
+        // zapisz i usuń wynik do listy,
+        // reset - czas i wynik
+        //
     }
 }
