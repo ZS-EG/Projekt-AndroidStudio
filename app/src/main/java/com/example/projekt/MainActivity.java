6975daf184d0a9a93efd1400a577784c91e04ff7
ZS-EG <grannygrandpacool@gmail.com>
2024-02-19 10:13:53 +0100

    dodane id i ulepszony wyglad

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -1,15 +1,51 @@
 package com.example.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonReset, buttonZapisz;
+    private ImageButton imageButton;
+    private ListView listaWynikow;
+    private ArrayList<String> wyniki = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //todo przycisk zmeinia pozycje po kazdym kliknieciu, liczenie punktow na kazdym kliknieciu, czas - 10 sekund, zapisz wynik,
+        buttonReset = findViewById(R.id.buttonRestart);
+        buttonZapisz = findViewById(R.id.buttonZapiszList);
+        imageButton = findViewById(R.id.imageButton);
+        listaWynikow = findViewById(R.id.listView);
+        buttonZapisz.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //zapis wyniku do listy
+                    }
+                }
+        );
+        buttonReset.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //reset wyniku
+                    }
+                }
+        );
+        imageButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //zmienia pozycje po kazdym kliknieciu
+                    }
+                }
+        );
+        //todo przycisk zmeinia pozycje po kazdym kliknieciu, liczenie punktow na kazdym kliknieciu, czas - 10 sekund, zapisz wynik do listy,
     }
 }
